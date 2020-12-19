c5e26f47c97dbc8dfe565528ed928f37bf90803f
chris666795 <76166921+chris666795@users.noreply.github.com>
2020-12-20 02:20:10 +0800

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int value[2];
+    for(int i=0;i<2;i++){
+        scanf("%d", &value[i]);
+    }
+    if(pow(value[0],2)+pow(value[1],2)<=100*100) printf("inside\n");
+    else printf("outside\n");
+    return 0;
+}
