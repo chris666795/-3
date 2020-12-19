05fde61101eecd796109d395f53f78ce3412223d
chris666795 <76166921+chris666795@users.noreply.github.com>
2020-12-20 00:49:36 +0800

    Add files via upload

---
+++ 12.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int recursive(int a){
+    if(a==1 || a==0) return a+1;
+    else return (recursive(a-1)+recursive(a/2));
+}
+
+int main(){
+    int num;
+    scanf("%d", &num);
+    printf("%d", recursive(num));
+    return 0;
+}
